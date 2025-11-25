b0dba3b6c9c87c6b690910c3de62edc3f771591c
Mark J Price <markjprice@gmail.com>
2025-11-25 13:14:50 +0000

    Update to Aspire 13

--- code/Chapter16/AspireNew/Northwind.WebApi/Program.cs
+++ code/Chapter16/AspireNew/Northwind.WebApi/Program.cs
@@ -3,12 +3,18 @@ var builder = WebApplication.CreateBuilder(args);
 builder.AddServiceDefaults();
 
 // Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
 app.MapDefaultEndpoints();
 
 // Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
 
 app.UseHttpsRedirection();
 
@@ -28,7 +34,8 @@ app.MapGet("/weatherforecast", () =>
         ))
         .ToArray();
     return forecast;
-});
+})
+.WithName("GetWeatherForecast");
 
 app.Run();
 
